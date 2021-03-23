24780d67baf1499c4aecc71c0ed79019fa637c46
Sanu Kumar <sanukumar@Sanus-MacBook-Air.local>
2021-03-23 16:02:28 +0530

    added cart

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import React from 'react';
+import CartItem from './CartItem';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+       Cart
+     </h1>
+     <CartItem/>
+    </div>
+  );
+}
+
+export default App;
